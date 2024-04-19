eff2b86ae8c107850f8aff9da10eaa0dac0580f8
CarlosSamirRoque <carlossamirroqueescobar@gmail.com>
2024-04-19 17:18:46 -0600

    Modificacion a tests en RepuestoTest
    modifique los 3 tests restantes

--- src/test/java/ventaderepuestos/test/RepuestoTest.java
+++ src/test/java/ventaderepuestos/test/RepuestoTest.java
@@ -12,72 +12,55 @@ import static java.time.Duration.ofSeconds;
 public class RepuestoTest {
 
     @Test
-    public void testGuardarRepuesto() throws InterruptedException {
-
-        // Inicializa el WebDriver para Chrome
+    public void testGuardarRepuesto() {
         WebDriver driver = new ChromeDriver();
-        //WebDriver driver = new FirefoxDriver();
-
         try {
-
-
-            // Abre la página web de inicio de sesión
             driver.get("http://localhost:8080/repuestos");
+            new WebDriverWait(driver, ofSeconds(30)).until(titleIs("Repuestos"));
 
-            new WebDriverWait (driver, ofSeconds(30), ofSeconds(1)).until(titleIs("Repuestos"));
+            WebElement campoNombreRepuesto = driver.findElement(By.id("txt_nombreRepuesto"));
+            WebElement campoMarca = driver.findElement(By.id("txt_marcaRepuesto"));
+            WebElement campoPrecio = driver.findElement(By.id("txt_precioUnitario"));
+            WebElement campoStock = driver.findElement(By.id("txt_unidades"));
+            WebElement campoProveedor = driver.findElement(By.id("txt_proveedor"));
+            WebElement campoEstado = driver.findElement(By.id("txt_estado"));
+            WebElement btnGuardar = driver.findElement(By.id("btn_guardar"));
 
-            //Espera 3mil segundos
-            Thread.sleep(3000);
-        WebElement campoNombreRepuesto = driver.findElement(By.xpath("//vaadin-text-field[@id='txt_nombreRepuesto']/input"));
-        WebElement campoMarca = driver.findElement(By.xpath("//vaadin-text-field[@id='txt_marcaRepuesto']/input"));
-        WebElement campoPrecio = driver.findElement(By.xpath("//vaadin-text-field[@id='txt_precioUnitario']/input"));
-        //WebElement campoStock = driver.findElement(By.xpath("//vaadin-number-field[@id='txt_unidades']/input"));
-        WebElement campoProveedor = driver.findElement(By.xpath("//vaadin-combo-box[@id='txt_proveedor']/input"));
-        //WebElement campoEstado = driver.findElement(By.xpath("//vaadin-text-field[@id='txt_estado']/input"));
-        WebElement btnGuardar = driver.findElement(By.xpath("//vaadin-button[@id='btn_guardar']"));
+            campoNombreRepuesto.sendKeys("Pastillas de freno");
+            campoMarca.sendKeys("Continental");
+            campoPrecio.sendKeys("2000");
+            campoStock.sendKeys("8");
+            campoProveedor.sendKeys("Continental");
+            campoEstado.sendKeys("Activo");
 
-        campoNombreRepuesto.sendKeys("Pastillas de freno");
-        campoMarca.sendKeys("Continental");
-        campoPrecio.sendKeys("2000");
-        //campoStock.sendKeys("8");
-        campoProveedor.sendKeys("Continental");
-        //campoEstado.sendKeys("Activo");
+            btnGuardar.click();
 
-        Thread.sleep(3000);
-
-        //btnGuardar.click();
-        // Aquí puedes agregar aserciones o verificaciones adicionales según sea necesario
-    }finally {
-            driver.close();
+            new WebDriverWait(driver, ofSeconds(10)).until(titleIs("Repuestos"));
+        } finally {
+            driver.quit();
         }
+    }
 
+    @Test
+    void testEliminarRepuesto() {
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("http://localhost:8080/repuestos");
+            new WebDriverWait(driver, ofSeconds(30)).until(titleIs("Repuestos"));
 
+            WebElement btnEliminar = driver.findElement(By.id("btn_eliminar"));
+            btnEliminar.click();
 
-//    @Test
-//    void testMostrarMensajeError() {
-//        repuestosView.mostrarMensajeError("Este es un mensaje de error");
-//        // Aquí puedes agregar aserciones o verificaciones adicionales según sea necesario
-//    }
-//
-//    @Test
-//    void testEliminarRepuesto() throws InterruptedException {
-//        driver.get("http://localhost:8080/repuestos");
-//        new WebDriverWait(driver, ofSeconds(30)).until(ExpectedConditions.titleIs("Repuestos"));
-//        Thread.sleep(3000);
-//
-//        // Simular la selección de un repuesto en el grid
-//        WebElement btnEliminar = driver.findElement(By.xpath("//vaadin-button[@id='btn_eliminar']"));
-//        btnEliminar.click();
-//
-//        // Verificar que se llame al método eliminarRepuesto en el controlador con el id del repuesto seleccionado
-//        verify(mockControlador).eliminarRepuesto("id_del_repuesto_seleccionado");
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
-}
+            // Verificar que se llame al método eliminarRepuesto en el controlador con el id del repuesto seleccionado
+ 
+        } finally {
+            driver.quit();
+        }
+    }
+
+    // TestMostrarMensajeError
+    @Test
+    void testMostrarMensajeError() {
+        // Simular el método mostrarMensajeError en la vista y verificar que se muestre correctamente el mensaje de error
+    }
 }
